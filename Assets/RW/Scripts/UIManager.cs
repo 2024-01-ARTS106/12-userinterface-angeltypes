092016aff2e446f41f4013d93bd568eaf8503bc0
angeltypes <157560431+angeltypes@users.noreply.github.com>
2024-04-22 19:42:49 -0400

    Wenderlich - part 3 COMPLETE
    i have been shrimp posturing. my back hurts. if you see this while grading things straighten your back a little bit and also maybe grab a snack. i should do that. weh

--- Assets/RW/Scripts/UIManager.cs
+++ Assets/RW/Scripts/UIManager.cs
@@ -9,6 +9,8 @@ public class UIManager : MonoBehaviour
     public Animator startButton;
     public Animator settingsButton;
     public Animator dialog;
+    public Animator contentPanel;
+    public Animator gearImage;
     public void OpenSettings() 
 {
     startButton.SetBool("isHidden", true);
@@ -21,6 +23,13 @@ public void CloseSettings()
     settingsButton.SetBool("isHidden", false);
     dialog.SetBool("isHidden", true);
 }
+public void ToggleMenu() 
+{
+    bool isHidden = contentPanel.GetBool("isHidden");
+    contentPanel.SetBool("isHidden", !isHidden);
+    gearImage.SetBool("isHidden", !isHidden);
+}
+
 
    public void StartGame()
    {
