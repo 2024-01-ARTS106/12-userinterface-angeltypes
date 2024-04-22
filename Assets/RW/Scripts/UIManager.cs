c78c4ee2db70fd9322267507fb4e834f75b3fc3b
angeltypes <157560431+angeltypes@users.noreply.github.com>
2024-04-22 18:39:38 -0400

    Wenderlich - part 2 complete
    i heart coding!!!! (sarcastic)(but also so geniune)

--- Assets/RW/Scripts/UIManager.cs
+++ Assets/RW/Scripts/UIManager.cs
@@ -4,8 +4,24 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
+
 {
-    
+    public Animator startButton;
+    public Animator settingsButton;
+    public Animator dialog;
+    public void OpenSettings() 
+{
+    startButton.SetBool("isHidden", true);
+    settingsButton.SetBool("isHidden", true);
+    dialog.SetBool("isHidden", false);
+}
+public void CloseSettings() 
+{
+    startButton.SetBool("isHidden", false);
+    settingsButton.SetBool("isHidden", false);
+    dialog.SetBool("isHidden", true);
+}
+
    public void StartGame()
    {
        SceneManager.LoadScene("RocketMouse");
